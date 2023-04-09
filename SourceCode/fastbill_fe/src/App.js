e54f5148043cf32fc3b0363edc91c7df4d70a614
phong2002 <phong.luminh02@gmail.com>
2023-04-09 21:06:49 +0700

    update : delete router path [signup,bank]

--- SourceCode/fastbill_fe/src/App.js
+++ SourceCode/fastbill_fe/src/App.js
@@ -1,10 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignInScreen from "./views/authen/SignIn";
-import SingUpScreen from "./views/authen/SignUp";
 import Home from "./views/administer/Home";
 import Payments from "./views/administer/Payments";
 import Profile from "./views/profile/Profile";
-import Banking from "./views/administer/Banking";
 import Register from "./views/administer/Register";
 import GeneralManagement from "./views/administer/GeneralManagement";
 import ElectricNumber from "./views/administer/ElectricNumber";
@@ -15,11 +13,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<SignInScreen />} />
-          <Route path="/signup" element={<SingUpScreen />} />
           <Route path="/home" element={<Home />} />
           <Route path="/payment" element={<Payments />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/bank" element={<Banking />} />
           <Route path="/register" element={<Register />} />
           <Route path="/general-management" element={<GeneralManagement />} />
           <Route path="/electric-number" element={<ElectricNumber />} />
