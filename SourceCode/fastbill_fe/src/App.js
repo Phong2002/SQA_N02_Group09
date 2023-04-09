b5e1225e246acf824558d88dfe854e7a2eeb35a4
phong2002 <phong.luminh02@gmail.com>
2023-04-09 18:49:49 +0700

    update : config router path in project

--- SourceCode/fastbill_fe/src/App.js
+++ SourceCode/fastbill_fe/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignInScreen from "./views/authen/SignIn";
+import SingUpScreen from "./views/authen/SignUp";
+import Home from "./views/administer/Home";
+import Payments from "./views/administer/Payments";
+import Profile from "./views/profile/Profile";
+import Banking from "./views/administer/Banking";
+import Register from "./views/administer/Register";
+import GeneralManagement from "./views/administer/GeneralManagement";
+import ElectricNumber from "./views/administer/ElectricNumber";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignInScreen />} />
+          <Route path="/signup" element={<SingUpScreen />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/payment" element={<Payments />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/bank" element={<Banking />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/general-management" element={<GeneralManagement />} />
+          <Route path="/electric-number" element={<ElectricNumber />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
