6e18cd557602c46a45fd5245d558593c6a095031
Chin <113930748+Nine-TT@users.noreply.github.com>
2023-04-27 16:54:01 +0700

    update config db file, Readme file

--- SourceCode/fastbill_be/src/controllers/authController.js
+++ SourceCode/fastbill_be/src/controllers/authController.js
@@ -27,7 +27,6 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
     try {
         const data = req.body
-        console.log(data);
 
         if (!data.email || !data.password) {
             return res.status(400).json({
