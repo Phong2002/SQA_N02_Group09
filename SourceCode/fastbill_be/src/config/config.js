b5da7d96aad3d0d9fd6fe4fb735c09d5dc525c56
phong2002 <phong.luminh02@gmail.com>
2023-05-23 17:37:05 +0700

    update config

--- SourceCode/fastbill_be/src/config/config.js
+++ SourceCode/fastbill_be/src/config/config.js
@@ -1,33 +1,36 @@
-require('dotenv').config({ path: "../../.env" });
+require('dotenv').config();
 
 
 module.exports = {
     development: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: "mysql",
-        logging: false,
-        timezone: "+07:00"
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.DB_HOST,
+        "port": process.env.DB_PORT,
+        "dialect": "mysql",
+        "timezone": "+07:00",
+        createDatabaseIfNotExist: true
     },
 
     test: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: "mysql",
-        logging: false,
-        timezone: "+07:00"
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.DB_HOST,
+        "port": process.env.DB_PORT,
+        "dialect": "mysql",
+        "timezone": "+07:00",
+        createDatabaseIfNotExist: true
     },
     production: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: "mysql",
-        logging: false,
-        timezone: "+07:00"
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.DB_HOST,
+        "port": process.env.DB_PORT,
+        "dialect": "mysql",
+        "timezone": "+07:00",
+        createDatabaseIfNotExist: true
     }
 }; 
