c08928f91b9d0eda973093964bdb1ede2c533b8d
phong2002 <phong.luminh02@gmail.com>
2023-05-23 17:37:32 +0700

    update connection db

--- SourceCode/fastbill_be/connection_db.js
+++ SourceCode/fastbill_be/connection_db.js
@@ -3,8 +3,14 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: 'mysql',
-  logging: false
+  port: process.env.DB_PORT,
+  dialect: "mysql",
+  logging: false,
+  query: {
+    "raw": true
+  },
+  timezone: "+07:00",
+  createDatabase: true
 });
 
 
@@ -12,8 +18,23 @@ const connection_db = async () => {
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
+    console.log(process.env.DB_HOST)
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    if (error.original.code === 'ER_BAD_DB_ERROR') { // kiểm tra nếu lỗi là do chưa có database
+      console.log('Database not found. Creating new database...');
+      const mysql = require('mysql2/promise');
+      const connection = await mysql.createConnection({ // tạo kết nối tạm thời để tạo mới database
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+      });
+      await connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`); // tạo mới database nếu chưa tồn tại
+      console.log(`Database ${process.env.DB_NAME} created successfully.`);
+      await connection.end(); // đóng kết nối tạm thời
+    } else {
+      console.error('Unable to connect to the database:', error);
+    }
   }
 }
 
